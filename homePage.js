694e2379e403c7e5add3634f9b471dc388bfb79b
anisotropies <Anisotropies@users.noreply.github.com>
2017-11-19 14:55:43 -0800

    Updated homepage with GitHub, Resume

--- homePage.js
+++ homePage.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
 
-	$('#meltStar').hover(function() {
-		$(this).fadeOut(500);
-		$(this).fadeIn(500);
-	});
-	
-	 $( "#meltStar" ).animate({
-    opacity: 0.25,
-    left: "+=50",
-    height: "toggle"
-  }, 5000, function() {
-    // Animation complete.
-  });
-  
+
 });
