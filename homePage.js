c8a33d858e4ce92c31d5b00a68c27d6664d8f554
Andrew Chan <Anisotropies@users.noreply.github.com>
2017-11-21 09:16:07 -0800

    Added button to initiate get request

--- homePage.js
+++ homePage.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+
+function UserAction() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET","http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/tas/2000/2010/USA" , true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
     var response = JSON.parse(xhttp.responseText);
     alert(response);
+}
 });
