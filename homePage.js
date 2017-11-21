d9ee73d82fd0bbc73549f830ab6e8208c74ec50f
Andrew Chan <Anisotropies@users.noreply.github.com>
2017-11-21 09:03:58 -0800

    Test xmlhttprequest.

--- homePage.js
+++ homePage.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
-
-
+    xhttp.open("GET","http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/tas/2000/2010/USA" , true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
+    alert(response);
 });
