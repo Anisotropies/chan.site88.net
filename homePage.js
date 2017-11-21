a2a067cecd15319fe068cfca8fe06fada3b6016d
Andrew Chan <Anisotropies@users.noreply.github.com>
2017-11-21 09:22:24 -0800

    Test alert--still not working!

--- homePage.js
+++ homePage.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-
+alert("hello!");
 function UserAction() {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET","http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/tas/2000/2010/USA" , true);
+    xhttp.open("GET","http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/tas/2000/2010/USA",false);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
     var response = JSON.parse(xhttp.responseText);
-    alert(response);
+    alert("xhttp.status: " + xhttp.status);
+    alert("response"+response);
 }
 });
